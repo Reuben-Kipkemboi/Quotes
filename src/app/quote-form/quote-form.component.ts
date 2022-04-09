c73809391f5d188ac7167749d9ae0990e2028055
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-09 11:49:19 +0300

    st:form - uses ngmodel displaying user input

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,11 +7,13 @@ import { Quotedetails } from '../quotedetails'; // importing our blueprint class
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  // help in creating a new instance of quote with the same properties
+  newQuote = new Quotedetails(0, "", 0, 0, "", "")
 
   userquote:Quotedetails[] = [
     new Quotedetails(1, "East or west home is best", 0, 0, 'Belinda kross', 'Reuby and Reuby'),
-    new Quotedetails(4, "East or west home is best", 0, 0, 'Jane Doe', 'Reuben Kipkemboi'),
-    new Quotedetails(17, "The marathon continues.....", 0, 0, 'The Great Nipsey Hussle', 'John Doe')
+    // new Quotedetails(4, "East or west home is best", 0, 0, 'Jane Doe', 'Reuben Kipkemboi'),
+    // new Quotedetails(17, "The marathon continues.....", 0, 0, 'The Great Nipsey Hussle', 'John Doe')
   ]; //creating of property using our class.
 
   constructor() { }
