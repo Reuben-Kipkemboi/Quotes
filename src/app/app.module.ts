55f7e113e13b09c1581cd5c9fefe568c8c2477c8
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-09 08:43:24 +0300

    ft:directive - creates a highlight directive to highlight highest voted quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightquoteDirective } from './highlightquote.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    HighlightquoteDirective
   ],
   imports: [
     BrowserModule,
